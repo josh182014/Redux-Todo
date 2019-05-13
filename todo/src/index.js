5aecc6bedf72cccd88b754d0b078b1d03d7a8705
josh182014 <josh182014@gmail.com>
2019-05-13 16:51:30 -0600

    moved store and provider from app.js to index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = () => {
+  return {
+    title: 'This is a title from the Redux store!!'
+  };
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+  </Provider>,
+document.getElementById('root'));
